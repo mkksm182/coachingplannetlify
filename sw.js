686a847d824dbe39f5f293f7dfa87152e00ebd2a
mkksm182 <michalik.maksymilian@gmail.com>
2026-06-17 15:08:17 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='op-coach-center-v1';
-const FILES=['./','./index.html','./style.css','./app.js','./data/plan.js','./manifest.json','./assets/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
